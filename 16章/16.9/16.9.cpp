33599b7bd702f1ebc782f094e44624c5239d4763
刘庆宗 <1045033409@qq.com>
2021-10-03 10:45:10 +0800

    完成16.19习题

---
+++ 16章/16.9/16.9.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <vector>
+template <typename Container>
+void print_16_19(const Container& c) {
+    for (typename Container::size_type i = 0; i != c.size(); i++) {
+        std::cout << c[i] << "    ";
+    }
+    std::cout << std::endl;
+}
+
+int main()
+{
+    auto vec = std::vector<int>{ 1,2,3,4,5,6 };
+    print_16_19(vec);
+}
